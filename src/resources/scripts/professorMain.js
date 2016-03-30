27dd07ca641a8b0184189b368f223bcefc79eb33
Valeriia Shpychka <valeriia.shpychka@ignite.ee>
2016-03-30 21:49:03 +0200

    table creation from JSON

--- src/resources/scripts/professorMain.js
+++ src/resources/scripts/professorMain.js
@@ -1,5 +1,12 @@
 var readonlyProperty = true;
 
+
+var studentsArray = '{ "students" : [' +
+    '{ "firstName":"John" , "lastName":"Doe", "email":"john.doe@ttu.ee", "subjectCode":"IDU0021", "grade":"3"},' +
+    '{ "firstName":"Anna" , "lastName":"Smith", "email":"anna.smith@ttu.ee","subjectCode":"IDU0021", "grade":"5" },' +
+    '{ "firstName":"Peter" , "lastName":"Jones", "email":"peter.jones@ttu.ee", "subjectCode":"IDU0021", "grade":"2"} ' +
+    ']}';
+
 $(document).ready(function () {
     $(function () {
         document.getElementById("addNewLine").style.visibility = 'hidden';
@@ -13,15 +20,73 @@ function loadContent(content) {
         window.location = "../login.html";
     }
     //disable others
-    document.getElementById('professorHome').style.display = 'none';
     document.getElementById('professorNotifications').style.display = 'none';
     document.getElementById('profEnterMarks').style.display = 'none';
     document.getElementById('professorMarks').style.display = 'none';
     document.getElementById('profStatistics').style.display = 'none';
     document.getElementById('professorSearch').style.display = 'none';
     //enable one
-    document.getElementById(content).style.display = 'block';
+    var div = document.getElementById(content);
+    div.style.display = 'block';
+    if (content === "professorNotifications") {
+        professorNotifications();
+    } else if (content === "profEnterMarks") {
+        profEnterMarks();
+    } else if (content === "professorMarks") {
+        professorMarks();
+    } else if (content === "profStatistics") {
+        professorStatistics();
+    }
+
 }
+
+function professorNotifications() {
+
+}
+
+function profEnterMarks() {
+
+}
+
+function professorMarks() {
+    var table = document.getElementById("marksProf");
+    table.tBodies[0].remove();
+    for (var i = 0; i < Object.keys(studentsArray).length - 1; i++) {
+        changedPage = false;
+        obj = JSON.parse(studentsArray);
+        var row = table.insertRow(1);
+        row.className = "info";
+        var studentName = row.insertCell(0);
+        var subjectCode = row.insertCell(1);
+
+
+        var grade = row.insertCell(2);
+        studentName.innerHTML = obj.students[i].firstName + " " + obj.students[i].lastName;
+        subjectCode.innerHTML = obj.students[i].subjectCode;
+        grade.innerHTML = obj.students[i].grade;
+    }
+
+}
+function professorStatistics() {
+    var table = document.getElementById("marksProf");
+    table.tBodies[0].remove();
+    for (var i = 0; i < Object.keys(studentsArray).length - 1; i++) {
+        changedPage = false;
+        obj = JSON.parse(studentsArray);
+        var row = table.insertRow(1);
+        row.className = "info";
+        var studentName = row.insertCell(0);
+        var subjectCode = row.insertCell(1);
+
+
+        var grade = row.insertCell(2);
+        studentName.innerHTML = obj.students[i].firstName + " " + obj.students[i].lastName;
+        subjectCode.innerHTML = obj.students[i].subjectCode;
+        grade.innerHTML = obj.students[i].grade;
+    }
+
+}
+
 
 function showInputForm() {
     document.getElementById("addNewLine").style.visibility = 'visible';
@@ -58,13 +123,13 @@ function validate() {
     }
 }
 var data = [
-    { value: "Teet Tee", label: "Teet Tee" },
-    { value: "Anna Puu", label: "Anna Puu" },
-    { value: "Siim Orav", label: "Siim Orav" },
-    { value: "Kadri Voi", label: "Kadri Voi" },
-    { value: "Mait Kass", label: "Mait Kass" }
+    {value: "Teet Tee", label: "Teet Tee"},
+    {value: "Anna Puu", label: "Anna Puu"},
+    {value: "Siim Orav", label: "Siim Orav"},
+    {value: "Kadri Voi", label: "Kadri Voi"},
+    {value: "Mait Kass", label: "Mait Kass"}
 ];
-$(function() {
+$(function () {
     $("#sdName").autocomplete({
         source: data
     });
